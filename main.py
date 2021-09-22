8f389c0d63eb73cb0723a961a6fa71fea38521d0
Plubbers <possaweekrish@gmail.com>
2021-09-22 16:39:08 +0700

    Link to discord test1

---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import os
+
+token = os.getenv('TOKEN')
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+  await client.change_presence(
+      status = discord.Status.idle,
+      activity = discord.Game('with probabilities...')
+    )
+  print('Raphael on standby...')
+
+@client.command()
+async def load(ctx, extension):
+  client.load_extension(f'cogs.{extension}')
+  await ctx.send(f'Sucessfully loaded {extension} cog')
+
+@client.command()
+async def unload(ctx, extension):
+  client.unload_extension(f'cogs.{extension}')
+  await ctx.send(f'Sucessfully unloaded {extension} cog')
+
+@client.command()
+async def reload(ctx, extension):
+  client.unload_extension(f'cogs.{extension}')
+  client.load_extension(f'cogs.{extension}')
+  await ctx.send(f'Sucessfully reloaded {extension} cog')
+
+for filename in os.listdir('./cogs'):
+  if filename.endswith('.py'):
+    client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run(token)
